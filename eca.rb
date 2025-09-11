40506788e4c01208907a3dbb670f471730fd6e99
Eric Dallo (ECA) <ercdll1337+eca@gmail.com>
2025-09-11 15:17:48 +0000

    Bump to version: 0.51.1

--- eca.rb
+++ eca.rb
@@ -1,25 +1,25 @@
 class Eca < Formula
   desc "Editor Code Assistant (ECA) - AI pair programming capabilities agnostic of editor"
   homepage "https://github.com/editor-code-assistant/eca"
-  version "0.51.0"
+  version "0.51.1"
 
   option "with-dynamic", "Installs the not static binary."
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/editor-code-assistant/eca/releases/download/0.51.0/eca-native-macos-aarch64.zip"
-      sha256 "2342c34ee9375cc7708aa72a8fd36cd29ccd96bf2a65bce8a30bf81481385bc9"
+      url "https://github.com/editor-code-assistant/eca/releases/download/0.51.1/eca-native-macos-aarch64.zip"
+      sha256 "826bc79052414bfeea80fc167cb3231a837467d4a6614bb178a5324de4567d34"
     else
-      url "https://github.com/editor-code-assistant/eca/releases/download/0.51.0/eca-native-macos-amd64.zip"
-      sha256 "b2c831cb09f4575af1d3ad8bf29f5b6833c69d41302b9b24c6887dae3757f3c4"
+      url "https://github.com/editor-code-assistant/eca/releases/download/0.51.1/eca-native-macos-amd64.zip"
+      sha256 "7d3419dd0c366cabaa8e21f3a8910c751dac5d1c803799bed234ec5d275eeabd"
     end
   elsif OS.linux?
     if build.with? "dynamic"
-      url "https://github.com/editor-code-assistant/eca/releases/download/0.51.0/eca-native-linux-amd64.zip"
-      sha256 "6cdadb028016b3f5f8d8bc75c060cc3e609c8e6a109a73bb03694a9a230caeae"
+      url "https://github.com/editor-code-assistant/eca/releases/download/0.51.1/eca-native-linux-amd64.zip"
+      sha256 "6f0dfa1c2066d6740cd2583b84c132256c83e07cb71ae209a12bf1bcb336f31c"
     else
-      url "https://github.com/editor-code-assistant/eca/releases/download/0.51.0/eca-native-static-linux-amd64.zip"
-      sha256 "ab3ce3df33e5564e0c4337a7df598ed6faa0b9d80107b7b6a142df1ed6946811"
+      url "https://github.com/editor-code-assistant/eca/releases/download/0.51.1/eca-native-static-linux-amd64.zip"
+      sha256 "49b8942c5e9bcca775af97e297d3c8444f5e6b1245a3a676416cfa769432d72f"
     end
   end
 
