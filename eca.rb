88216cf0861fd93fdb653d1f03b5e28011ee1893
Eric Dallo (ECA) <ercdll1337+eca@gmail.com>
2026-04-07 14:29:56 +0000

    Bump to version: 0.124.1

--- eca.rb
+++ eca.rb
@@ -1,25 +1,25 @@
 class Eca < Formula
   desc "Editor Code Assistant (ECA) - AI pair programming capabilities agnostic of editor"
   homepage "https://github.com/editor-code-assistant/eca"
-  version "0.124.0"
+  version "0.124.1"
 
   option "with-dynamic", "Installs the not static binary."
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/editor-code-assistant/eca/releases/download/0.124.0/eca-native-macos-aarch64.zip"
-      sha256 "242a44d81bf860580dbd434df28fd4e9cc0e16becda833899a12c1e58fb2d765"
+      url "https://github.com/editor-code-assistant/eca/releases/download/0.124.1/eca-native-macos-aarch64.zip"
+      sha256 "427362a9d20537cfa2ce2681a187e42b55f4d2b8e9afb3b0a02443e78f845783"
     else
-      url "https://github.com/editor-code-assistant/eca/releases/download/0.124.0/eca-native-macos-amd64.zip"
-      sha256 "67ed2323673d6f009c4df89ed0aa4960e34a910bff535bc173bd074c7a5d5af3"
+      url "https://github.com/editor-code-assistant/eca/releases/download/0.124.1/eca-native-macos-amd64.zip"
+      sha256 "bddbcc32ce86b2c92df590a61d909ab303d0472020cff271ed4f513696725034"
     end
   elsif OS.linux?
     if build.with? "dynamic"
-      url "https://github.com/editor-code-assistant/eca/releases/download/0.124.0/eca-native-linux-amd64.zip"
-      sha256 "e4b0d9daab5f48468836de9d4bbe0b9c27b59f088b1df3165103e3c5011f21f1"
+      url "https://github.com/editor-code-assistant/eca/releases/download/0.124.1/eca-native-linux-amd64.zip"
+      sha256 "7e6a059d22f666f69bfc7789e2286f41dc716f03d3e7129a7136f8e120de0626"
     else
-      url "https://github.com/editor-code-assistant/eca/releases/download/0.124.0/eca-native-static-linux-amd64.zip"
-      sha256 "1afdc912c942a9339430b979cedd7804a8699377a154a1fb6ae20a885b0bdd68"
+      url "https://github.com/editor-code-assistant/eca/releases/download/0.124.1/eca-native-static-linux-amd64.zip"
+      sha256 "56a6af6d8b43d8a73efdf3af79daf87c7c2f21af2932a961fc2ef6b4a3a648a7"
     end
   end
 
