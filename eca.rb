5fe1e5b8215916f3211d24ad21d9eabdc8e1332a
Eric Dallo (ECA) <ercdll1337+eca@gmail.com>
2026-06-17 13:54:44 +0000

    Bump to version: 141.0

--- eca.rb
+++ eca.rb
@@ -1,25 +1,25 @@
 class Eca < Formula
   desc "Editor Code Assistant (ECA) - AI pair programming capabilities agnostic of editor"
   homepage "https://github.com/editor-code-assistant/eca"
-  version "0.140.1"
+  version "141.0"
 
   option "with-dynamic", "Installs the not static binary."
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/editor-code-assistant/eca/releases/download/0.140.1/eca-native-macos-aarch64.zip"
-      sha256 "00c5356d9b9c4c1507fc9ab7f6826c3c082a1cf97657e828829114b710daa404"
+      url "https://github.com/editor-code-assistant/eca/releases/download/141.0/eca-native-macos-aarch64.zip"
+      sha256 "16cadfbe440d9a8cb4a586fda2e4f2ae644153b590845ef2a93a7e06d24750da"
     else
-      url "https://github.com/editor-code-assistant/eca/releases/download/0.140.1/eca-native-macos-amd64.zip"
-      sha256 "2de3f2c76435459dd651bd9b9e4926cc5ded943e831dec9f3245090fac014f89"
+      url "https://github.com/editor-code-assistant/eca/releases/download/141.0/eca-native-macos-amd64.zip"
+      sha256 "e31ff52379abdb36624988922530ec73070a7552d15fcb0cabc36260cc13fc6d"
     end
   elsif OS.linux?
     if build.with? "dynamic"
-      url "https://github.com/editor-code-assistant/eca/releases/download/0.140.1/eca-native-linux-amd64.zip"
-      sha256 "e4749b9aa04f7f09f6182581f2e86df476b932e59af479e504554d8d0a3c6f65"
+      url "https://github.com/editor-code-assistant/eca/releases/download/141.0/eca-native-linux-amd64.zip"
+      sha256 "dbb7272f7dcc4c2e879450e37e3fc791087cec8e90d61c7a49aede5fa0bafa6c"
     else
-      url "https://github.com/editor-code-assistant/eca/releases/download/0.140.1/eca-native-static-linux-amd64.zip"
-      sha256 "ad834f9b476cc27c86d5a9cec04b69de09107a985a4f0e158bd01886e35a27c5"
+      url "https://github.com/editor-code-assistant/eca/releases/download/141.0/eca-native-static-linux-amd64.zip"
+      sha256 "1644ad930250ffaa9066d118503e1461769c749fb7a068881499a82f7eb435f5"
     end
   end
 
